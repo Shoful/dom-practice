7b919fa03bdc4f4bb2b298264dec63473d8a700c
Shoful Alom <alom.shoful@gmail.com>
2021-10-17 20:27:51 -0400

    completed

--- script.js
+++ script.js
@@ -7,29 +7,67 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`,
 ];
 
-window.onload =  function(event) {
-  
+window.onload = function (event) {
+
   // Random quote of the day generator
-  const randomQuote = function() {
-    document.querySelector('#quote-of-the-day').textContent = `"${
-      quotes[Math.floor(Math.random() * quotes.length)]
-    }"`;
+  const randomQuote = function () {
+    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]
+      }"`;
   };
   randomQuote();
 
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  function changeMainID() {
+    document.getElementById("main-title").innerHTML = "DOM Toretto Quotes";
+  }
+  changeMainID();
   // Part 2
-
+  function changeBackgroundColor() {
+    document.body.style.backgroundColor = "#FA8072";
+  }
+  changeBackgroundColor();
   // Part 3
-
+  function removeLast() {
+    let favoritethings = document.getElementById("favorite-things");
+    favoritethings.removeChild(favoritethings.lastElementChild);
+  }
+  removeLast();
   // Part 4
-
+  function changeFontSize() {
+    let title = document.getElementsByClassName("special-title");
+    for (let i = 0; i < title.length; i++) {
+      title[i].style.fontSize = "2rem";
+    }
+  }
+  changeFontSize();
   // Part 5
-
+  function removeChicago() {
+    let cities = document.getElementById("past-races");
+    cities.removeChild(cities.children[3]);
+  }
+  removeChicago();
   // Part 6
-
+  function addingToPastRaces() {
+    let newcity = document.createElement('li');
+    newcity.textContent = "New York City";
+    document.getElementById("past-races").appendChild(newItem);
+  }
+  addingToPastRaces();
   // Part 7
+  function newBlogPost() {
+    const text = document.createElement("p");
+    const title = document.createElement("h1");
+    const block = document.createElement("div");
+    text.textContent = "I jumped off a cliff and then flew to space."
+    title.textContent = "New York City"
+    block.className = "blog-post purple"
+    block.appendChild(title)
+    block.appendChild(text)
+
+    let main = document.querySelector('.main')
+    main.appendChild(block)
+  }
+  newBlogPost()
 }
